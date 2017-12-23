bc07246d33e3eb5d3c661cda92738fd6aa184e7b
Anwesh43 <anweshthecool0@gmail.com>
2017-12-24 00:57:53 +0530

    using ColorSwiperView in Kotlin

--- app/src/main/java/ui/anwesome/com/kotlincolorswiperview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlincolorswiperview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlincolorswiperview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.colorswiperview.SwiperView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SwiperView.create(this)
     }
 }

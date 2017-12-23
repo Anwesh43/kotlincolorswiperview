6ab3e0c7df449494c11dc3bfd055af1198fdb606
Anwesh43 <anweshthecool0@gmail.com>
2017-12-23 23:05:57 +0530

    adding Screen

--- colorswiperview/src/main/java/ui/anwesome/com/colorswiperview/SwiperView.kt
+++ colorswiperview/src/main/java/ui/anwesome/com/colorswiperview/SwiperView.kt
@@ -14,4 +14,24 @@ class SwiperView(ctx:Context):View(ctx) {
     override fun onTouchEvent(event:MotionEvent):Boolean {
         return true
     }
+    data class Screen(var w:Float,var h:Float = 0f,var x:Float = 0f,var prevX:Float = 0f,var j:Float = 0f){
+        fun update(updateDb:(Int)->Unit) {
+            val updateFn:(Float,Float,Int)->Unit = { scale,dirf,dir ->
+                x = prevX + dir*scale*w
+                if(dirf == 0f) {
+                    x = prevX+dir*w
+                    prevX = x
+                    j+=dir
+                }
+            }
+        }
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(x,0f)
+            canvas.restore()
+        }
+        fun startUpdating(dir:Int) {
+
+        }
+    }
 }
